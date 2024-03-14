49708e7ed588957fbcaf499a3d851bdca06079e8
iamlemuelle <lemuellebagay3@gmail.com>
2024-03-14 11:02:01 +0800

    Delete Task

--- routes/web.php
+++ routes/web.php
@@ -42,13 +42,16 @@ Route::get('/tasks/{task}', function (Task $task) {
     ]);
 })->name('tasks.show');
 
+# CREATE A TASK
 Route::post('/tasks', function (TaskRequest $request) {
+
     $task = Task::create($request->validated());
 
     return redirect()->route('tasks.show', ['task' => $task->id])
     ->with('success', 'Task created successfully');
 })->name('tasks.store');
 
+# EDIT A TASK
 Route::put('/tasks/{task}', function (Task $task, TaskRequest $request) {
     // $data = ;
     // $task->title = $data['title'];
@@ -61,6 +64,14 @@ Route::put('/tasks/{task}', function (Task $task, TaskRequest $request) {
     return redirect()->route('tasks.show', ['task' => $task->id])
     ->with('success', 'Task updated successfully');
 })->name('tasks.update');
+
+# DELETE A TASK
+Route::delete('/tasks/{task}', function (Task $task){
+    $task->delete();
+
+    return redirect()->route('tasks.index')
+        ->with('success', 'Task deleted successfully');
+})->name('tasks.destroy');
 
 Route::fallback(function () {
     return 'Still got somewhere!';
